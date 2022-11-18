bed0fca4f66c8724ad66ddbc1d64e0dcf04b20f9
Catalina Acosta Rivera <catalina.acosta.rivera@gmail.com>
2022-11-18 12:13:59 +0100

    working version

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,52 @@
+class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
+  
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+end
+
+# private
+
+# def set_restaurant
+#   @restaurant = Restaurant.find(params[:restaurant_id])
+# end
+
+# def review_params
+#   params.require(:review).permit(:content, :rating)
+# end
+# end
